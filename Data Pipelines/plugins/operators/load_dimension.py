3f6b698e14665c8bde1efd04d1d23ec92a38421b
quochuy17 <81853358+quochuy17@users.noreply.github.com>
2022-10-10 23:07:49 +0700

    Update load_dimension.py

--- Data Pipelines/plugins/operators/load_dimension.py
+++ Data Pipelines/plugins/operators/load_dimension.py
@@ -5,18 +5,42 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    base_sql = '''
+    INSERT INTO {}
+    (
+    {}
+    )
+    '''
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 delimiter=",",
+                 ignore_headers=1,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.table = table
+        self.redshift_conn_id = redshift_conn_id
+        self.delimiter = delimiter
+        self.ignore_headers = ignore_headers
+        self.aws_credentials_id = aws_credentials_id
 
     def execute(self, context):
         self.log.info('LoadDimensionOperator not implemented yet')
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+        
+        self.log.info("Copying data from staging to destination in Redshift")
+        insert_stmt = self.table + '_table_insert'
+        
+        formatted_sql = LoadDimensionOperator.base_sql.format(
+            self.table,
+            SqlQueries.insert_stmt
+        )
+        elf.log.info(formatted_sql)
+        redshift_hook.run(formatted_sql)
