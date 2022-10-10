8b6df5c476e80eb9d3e3ddf7e3fec719f47328ac
quochuy17 <81853358+quochuy17@users.noreply.github.com>
2022-10-10 23:07:26 +0700

    Update load_fact.py

--- Data Pipelines/plugins/operators/load_fact.py
+++ Data Pipelines/plugins/operators/load_fact.py
@@ -20,3 +20,17 @@ class LoadFactOperator(BaseOperator):
 
     def execute(self, context):
         self.log.info('LoadFactOperator not implemented yet')
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run("DELETE FROM {}".format(self.table))
+        
+        self.log.info("Copying data from staging to destination in Redshift")
+        insert_stmt = self.table + '_table_insert'
+        
+        formatted_sql = LoadDimensionOperator.base_sql.format(
+            self.table,
+            SqlQueries.insert_stmt
+        )
+        elf.log.info(formatted_sql)
+        redshift_hook.run(formatted_sql)
